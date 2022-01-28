15c66e27c114f579ce46e06d95847e016d694c53
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-01-29 00:11:18 +0900

    fix settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,8 @@ module.exports = {
   printWidth: 100,
   singleQuote: true,
   trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  endOfLine: "lf",
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
   ],
 };
