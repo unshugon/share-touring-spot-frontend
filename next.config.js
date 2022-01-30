cc59a316d5b5369751ab3eede581fcdebeb72714
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-01-30 18:20:08 +0900

    modify favicon switch by env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    FAVICON_URL: process.env.NODE_ENV === `development` ? `dev_favicon.svg` : `prod_favicon.svg`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
