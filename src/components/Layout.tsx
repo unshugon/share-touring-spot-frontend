f7bba69d28bad193f1fba8e24e162de3e4327c7d
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-01-30 18:18:06 +0900

    add favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/function-component-definition */
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Header from './organisms/Header';
 import Modal from './organisms/Modal';
@@ -9,6 +10,7 @@ type Props = {
   children: React.ReactNode;
 };
 const Layout: React.FC<Props> = ({ children }: Props) => {
+  const { asPath } = useRouter();
   const [open, setOpen] = useState<boolean>(false);
   const toggleModalOpen = () => {
     setOpen((prev) => !prev);
@@ -17,10 +19,11 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
     <div>
       <Head>
         <title>Share Touring Spot</title>
+        <link rel="icon" href={process.env.FAVICON_URL} />
         <meta name="description" content="ツーリングスポット共有サイトです。" />
       </Head>
       <header>
-        <Header toggleModalOpen={toggleModalOpen} />
+        {!asPath.match(/auth\/signin/) && <Header toggleModalOpen={toggleModalOpen} />}
       </header>
       <main className="min-h-screen">{children}</main>
       <Modal open={open} setOpen={setOpen}>
