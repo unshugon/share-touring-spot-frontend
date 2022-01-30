d3446ebe6bbd6b0e6293a2066166b49f26b8c11b
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-01-30 18:21:28 +0900

    fix require content

--- src/components/organisms/PostNew.tsx
+++ src/components/organisms/PostNew.tsx
@@ -82,6 +82,7 @@ const PostNew: React.FC<Props> = ({ toggleModalOpen }: Props) => {
               rows={3}
               className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               placeholder="例: 絶景です"
+              required
               {...descriptionProperties}
             />
           </div>
