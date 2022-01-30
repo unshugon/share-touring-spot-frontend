77bf9f3266e701660ca00a744eea2b4b7e4f4b2c
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-01-30 14:51:59 +0900

    add type

--- utils/type.ts
+++ utils/type.ts
@@ -1,7 +1,21 @@
+export type User = {
+  userId: string;
+  username: string;
+  email: string;
+};
+
 export type Post = {
   id: string;
   title: string;
   content: string;
   image: string;
-  created_by: any;
+  created_by: User | null;
 };
+
+export type PostForm = {
+  title: string;
+  content: string;
+  images: File[];
+};
+
+export type SessionStatus = 'authenticated' | 'loading' | 'unauthenticated';
