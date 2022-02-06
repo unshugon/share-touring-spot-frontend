5cb260bcc8e446aa4299fa4478e3296b3b23a2a8
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-02-06 17:31:15 +0900

    add swr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,54 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable react/function-component-definition */
 import type { GetStaticProps, NextPage } from 'next';
+import { useEffect } from 'react';
+import useSWR, { Fetcher, Key } from 'swr';
 import { API_BASE_URL } from '../../utils/constants';
 import { Post } from '../../utils/type';
+import { getAllPosts } from '../api/getPosts';
 import PostCard from '../components/molecules/PostCard';
 
 type Props = {
-  posts: Post[];
+  fallbackPosts: Post[];
 };
 
-const Home: NextPage<Props> = ({ posts }: Props) => (
-  <div className="min-h-screen bg-white dark:bg-slate-900">
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-300">
-        ツーリングスポット一覧
-      </h2>
+const fetcher: Fetcher<Post[]> = (url: string) =>
+  fetch(`${API_BASE_URL}${url}`).then((res) => res.json());
+const apiUrl: Key = '/posts/spots/list/';
 
-      <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}
+const Home: NextPage<Props> = ({ fallbackPosts: staticPosts }: Props) => {
+  const { data: posts, mutate } = useSWR<Post[]>(apiUrl, fetcher, {
+    fallbackData: staticPosts,
+  });
+  useEffect(() => {
+    mutate();
+  }, [mutate]);
+  if (!posts) {
+    return <div>loading</div>;
+  }
+  return (
+    <div className="min-h-screen bg-white dark:bg-slate-900">
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-300">
+          ツーリングスポット一覧
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(`${API_BASE_URL}/posts/spots/list/`);
-  const posts = (await res.json()) as Post[];
+  const staticPosts = await getAllPosts();
 
   return {
     props: {
-      posts,
+      posts: staticPosts,
     },
     revalidate: 3,
   };
