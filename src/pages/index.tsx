3e72d38e05561eb9a0d5d94354d07bed337fb6d5
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-02-13 04:43:01 +0900

    fix lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,7 @@ const Home: NextPage<Props> = ({ fallbackPosts: staticPosts }: Props) => {
   useEffect(() => {
     mutate();
   }, [mutate]);
+
   return (
     <div className="min-h-screen bg-white dark:bg-slate-900">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
