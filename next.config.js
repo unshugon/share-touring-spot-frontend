425255759061a4285f9279cb0493d0553f25a7ea
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-02-12 22:59:10 +0900

    fix google account icon

--- next.config.js
+++ next.config.js
@@ -8,7 +8,13 @@ const nextConfig = {
     domains: [
       'share-touring-spot-api.herokuapp.com',
       'share-touring-spot-api-assets.s3.amazonaws.com',
-      'googleusercontent.com',
+      'storage.googleapis.com',
+      'lh1.googleusercontent.com',
+      'lh2.googleusercontent.com',
+      'lh3.googleusercontent.com',
+      'lh4.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      'lh6.googleusercontent.com',
     ],
   },
 };
