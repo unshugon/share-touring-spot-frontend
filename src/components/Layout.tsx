bb45a3235be40249227fcfdae03a805c0f0eb372
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-02-12 22:49:39 +0900

    add SEO measures

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -24,16 +24,26 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
     <div>
       <Head>
         <title>Share Touring Spot</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta property="og:url" content="https://www.share-touring-spot.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Share Touring Spot" />
+        <meta property="og:description" content="ツーリングスポット共有サイトです。" />
+        <meta property="og:site_name" content="ツーリングスポット共有サイトです。" />
+        <meta property="og:image" content="prod_favicon.svg" />
         <link rel="icon" href={process.env.FAVICON_URL} />
+        <link rel="apple-touch-icon" href={process.env.FAVICON_URL} />
         <meta name="description" content="ツーリングスポット共有サイトです。" />
       </Head>
       <header>
         {!asPath.match(/auth\/signin/) && <Header toggleModalOpen={toggleModalOpen} />}
       </header>
-      <main className="min-h-screen">{children}</main>
-      <Modal open={open} setOpen={setOpen}>
-        <PostNew toggleModalOpen={toggleModalOpen} />
-      </Modal>
+      <main className="min-h-screen">
+        {children}
+        <Modal open={open} setOpen={setOpen}>
+          <PostNew toggleModalOpen={toggleModalOpen} />
+        </Modal>
+      </main>
       <footer className="text-center dark:bg-slate-900 dark:text-gray-300">
         Share Touring Spot
       </footer>
